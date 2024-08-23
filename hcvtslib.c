21410561fd5e8f915249c05ace15c2a5cbeee4d9
hanyuu <hanyuujp5192@protonmail.com>
2024-08-23 23:58:43 +0100

    added function declaration notes

--- hcvtslib.c
+++ hcvtslib.c
@@ -1,1 +1,10 @@
 #include <stdio.h>
+
+//cursor_move_up()
+//cursor_move_down()
+//cursor_move_forward()
+//cursor_move_backward()
+//cursor_move_nextline()
+//cursor_move_prevline()
+//cursor_horizontal()
+//cursor_vertical()
