7f748934af42303b08fb231836aca69c53c67cae
hanyuu <hanyuujp5192@protonmail.com>
2024-08-24 23:41:10 +0100

    created header file

---
+++ hcvtslib.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// cursor positioning
+
+void cursor_move_up(int num)
+void cursor_move_down(int num)
+void cursor_move_forward(int num)
+void cursor_move_backward(int num)
+void cursor_move_nextline(int num)
+void cursor_move_prevline(int num)
+void cursor_horizontal(int num)
+void cursor_vertical(int num)
+void cursor_pos(int x, int y)
+
+// cursor visibility
+
+// text modification
