1bf340860505c8b0c9a063ace913c3bbcd4ea80d
hanyuu <hanyuujp5192@protonmail.com>
2024-08-29 23:50:06 +0100

    added more cursor functions

--- src/hcvtslib.c
+++ src/hcvtslib.c
@@ -6,13 +6,11 @@ void cursor_move_up(int num) { printf("\e[%dA", num); }
 void cursor_move_down(int num) { printf("\e[%dB", num); }
 void cursor_move_forward(int num) { printf("\e[%dC", num); }
 void cursor_move_backward(int num) { printf("\e[%dD", num); }
-/*
-void cursor_move_nextline(int num) {}
-void cursor_move_prevline(int num) {}
-void cursor_horizontal(int num) {}
-void cursor_vertical(int num) {}
-void cursor_pos(int x, int y) {}
-*/
+void cursor_move_prevline(int num) { printf("\e[%dE", num); }
+void cursor_move_nextline(int num) { printf("\e[%dF", num); }
+void cursor_horizontal(int num) { printf("\e[%dG", num); }
+void cursor_vertical(int num) { printf("\e[%dd", num); }
+void cursor_pos(int x, int y) { printf("\e[%d;%dH", x, y); }
 
 // cursor visibility
 
--- src/hcvtslib.h
+++ src/hcvtslib.h
@@ -4,25 +4,14 @@
 
 void cursor_move_up(int num);
 void cursor_move_down(int num);
-/*
-cursor_move_forward(int num);
-cursor_move_backward(int num);
-cursor_move_nextline(int num);
-cursor_move_prevline(int num);
-cursor_horizontal(int num);
-cursor_vertical(int num);
-cursor_pos(int x, int y);
-*/
 
-cursor_move_up(int num)
-cursor_move_down(int num)
-cursor_move_forward(int num)
-cursor_move_backward(int num)
-cursor_move_nextline(int num)
-cursor_move_prevline(int num)
-cursor_horizontal(int num)
-cursor_vertical(int num)
-cursor_pos(int x, int y)
+void cursor_move_forward(int num);
+void cursor_move_backward(int num);
+void cursor_move_nextline(int num);
+void cursor_move_prevline(int num);
+void cursor_horizontal(int num);
+void cursor_vertical(int num);
+void cursor_pos(int x, int y);
 
 // cursor visibility
 
